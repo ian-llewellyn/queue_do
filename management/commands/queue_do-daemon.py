424fdab815d91bb368cb5c20b6a4b2ac52f318f0
Ian Llewellyn <ian_llewellyn@hotmail.com>
2013-04-10 15:43:48 +0100

    Somewhat working version. Changed models to 1024 char field for input file. Lots of work on the daemon process.

--- management/commands/queue_do-daemon.py
+++ management/commands/queue_do-daemon.py
@@ -1,32 +1,45 @@
 from queue_do.models import Configuration, JobQueue
 import sys # exit
 import subprocess # call
+import time # sleep
+import select # select
 
-#inotifywait_cmd = 'inotifywait'
-#inotifywait_args = '-m -e CLOSE_WRITE -e MOVE_TO'
-inotifywait_cmd = 'echo'
-inotifywait_args = ''
+CPUS = 4
+MAIN_LOOP_SLEEP_TIME = 1
+
+inotifywait_cmd = 'inotifywait'
+inotifywait_args = '--quiet --monitor --event CLOSE_WRITE --event MOVED_TO --format %w%f'
+#inotifywait_cmd = 'echo'
+#inotifywait_args = '-q -m -e CLOSE_WRITE -f %f\ %w'
 
 class InotifyWait:
-    def __init__(self, *args, **kwargs):
+    INOTIFYWAIT_READLINE_TIMEOUT = 0.1
+
+    def __init__(self, configuration=None):
         # args may be a string: 'string',
         # ... many strings: 'a', 'b', 'c',
         # ... a list of strings: ['a', 'b', 'c']
         # ... or a combination of these.
-        self.watch_dirs = []
-        for arg in args:
-            if type(arg) == list:
-                self.watch_dirs += arg
-            if type(arg) == str:
-                self.watch_dirs.append(arg)
+        #self.watch_dirs = []
+        #for arg in args:
+        #    if type(arg) == list:
+        #        self.watch_dirs += arg
+        #    if type(arg) == str:
+        #        self.watch_dirs.append(arg)
 
-        for arg in kwargs:
-            # filter
-            if kwargs.has_key('filter'):
-                self.filter = kwargs['filter']
-            # configuration
-            if kwargs.has_key('configuration'):
-                self.configuration = kwargs['configuration']
+        # filter
+        #if kwargs.has_key('filter'):
+        #    self.filter = kwargs['filter']
+
+        # configuration
+        if configuration:
+            self.configuration = configuration
+        else:
+            return None
+            #self.filter = self.configuration.filter
+            #self.watch_dir = self.configuration.watch_dir
+            #self.run_script = self.configuration.run_script
+            #self.default_priority = self.configuration.default_priority
 
         # Start 'er up...
         self.start()
@@ -34,46 +47,124 @@ class InotifyWait:
     def start(self):
         popen_args_tuple = (inotifywait_cmd,)
         popen_args_tuple += tuple(inotifywait_args.split())
-        popen_args_tuple += tuple(self.watch_dirs)
-        # popen_args_tuple += self.watch_dirs
-        self.process = subprocess.Popen(popen_args_tuple), stdout=subprocess.PIPE)
+        if self.configuration.filter:
+            # ^.*\/\..*$ - This might be greedy and pick a hidden directory instead.
+            # ^.*\/\.[^\/]*$ - Ensure we are operating on the file part of the input.
+            popen_args_tuple += ('--exclude', self.configuration.filter)
+        popen_args_tuple += tuple(self.configuration.watch_dir.split(', '))
+        #print popen_args_tuple
+        self.process = subprocess.Popen(popen_args_tuple, stdout=subprocess.PIPE)
+
+    def stop(self):
+        self.process.terminate()
+
+    def readline(self):
+        # Two options here to stop the function blocking:
+        # 1. Use signal.alarm and register a dummy function
+        # 2. Use select.select with a timeout
+        (rlist, wlist, xlist) = select.select([self.process.stdout], [], [],
+            self.INOTIFYWAIT_READLINE_TIMEOUT)
+        if rlist:
+            #return self.process.stdout.readline()
+            return rlist[0].readline()
+        return ''
+
+class Processor(dict):
+    # 0 when idle, PID otherwise
+    CPU_IDLE_FLAG = None
+    def __init__(self, **kwargs):
+        self.__setitem__(0, self.CPU_IDLE_FLAG)
+        if kwargs.has_key('cpus'):
+            for i in range(kwargs['cpus']):
+                self.__setitem__(i, self.CPU_IDLE_FLAG)
+
+    def available(self):
+        for cpu in self.keys():
+            if self[cpu] == self.CPU_IDLE_FLAG:
+                return True
+            elif not self[cpu]['process'].poll():
+                # Process is still running
+                continue
+            elif self[cpu]['process'].poll() == 0:
+                # The job completed successfully
+                # Update job to Success status and save
+                self[cpu]['job'].status = 'success'
+                self[cpu]['job'].save()
+                self[cpu] = self.CPU_IDLE_FLAG
+                return True
+            else:
+                # Failure of some sort
+                # Update job to Failed status and save
+                self[cpu]['job'].status = 'fail'
+                self[cpu]['job'].save()
+                self[cpu] = self.CPU_IDLE_FLAG
+                return True
+        return False
+
+    def start_job(self, job):
+        # Find the first unused CPU
+        for cpu, pid in self.items():
+            if pid == self.CPU_IDLE_FLAG:
+                break
+        # cpu is the processor to use
+        popen_args_tuple = tuple(job.configuration.run_script)
+        popen_args_tuple += (job.input_file,)
+        try:
+            job.status = 'processing'
+            self.__setitem__(cpu, {'job': job,
+                'process': subprocess.Popen(popen_args_tuple)})
+        except:
+            job.status = 'fail'
+        finally:
+            job.save()
+
+## Setup the processor flags:
+processor = Processor(cpus=CPUS)
 
 ## Read configuration from DB
 inotifys = []
-for configuration in Configuration.object.all():
-    inotifys.append(InotifyWait(configuration.watch_dir, configuration=configuration))
+for configuration in Configuration.objects.all():
+    inotifys.append(InotifyWait(configuration=configuration))
 
-## Launch inotifywait | queue_do-add.py
-for inotify in inotifys:
-    inotify.start()
+## Launch inotifywait
+#for inotify in inotifys:
+#    inotify.start()
 
-for inotify in inotifys:
-    end_of_file = False
-    while not end_of_file:
-        msg = inotify.readline().strip()
-        if msg == '':
-            end_of_file = True
+running = True
+## MAIN LOOP
+while running:
+    ## QUQUE INCOMING FILES - Read input (if any) from inotify instances
+    for inotify in inotifys:
+        #print 'Checking inotifywait with PID: %s' % inotify.process.pid
+        end_of_file = False
+        while not end_of_file:
+            msg = inotify.readline().strip()
+            #print '[%s]' % msg
+            if msg == '':
+                end_of_file = True
+                break
+            jq = JobQueue()
+            jq.input_file = msg
+            jq.priority = inotify.configuration.default_priority
+            jq.configuration = inotify.configuration
+            jq.status = 'queued'
+            jq.save()
+
+
+    ## PROCESS JOBS IN QUEUE
+    status_sel = ['queued']
+    # FIXME: Check to see if priority is the dominating ORDER BY - that is what we want.
+    jobs = JobQueue.objects.filter(status__in = status_sel) \
+        .order_by('added') \
+        .order_by('priority')
+
+    for job in jobs:
+        if processor.available():
+            # Start the job
+            processor.start_job(job)
+        else:
+            # No processors are available
             break
-        jq = JobQueue()
-        jq.input_file = msg
-        jq.priority = inotfiy.configuration.default_priority
-        jq.status = 'queued'
-        jq.save()
+    # All CPUs in use or all jobs started
 
-
-status_sel = ['queued']
-jobs = JobQueue.objects.filter(status__in = status_sel) \
-         .order_by('added') \
-         .order_by('priority')
-
-for job in jobs:
-    if availability:
-        job.start()
-
-
-
-
-
-
-
-
+    time.sleep(MAIN_LOOP_SLEEP_TIME)
--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Configuration(models.Model):
     filter = models.CharField(max_length=100, blank=False, null=False,
         help_text='Regular expression to stop certain entries making it to the queue.')
     run_script = models.CharField(max_length=100, blank=False, null=False,
-        help_text='The script that should be run when the job is being processed.')
+        help_text='The script that should be run when the job is processed. The detected file will be passed in as the last argument.')
     default_priority = models.PositiveIntegerField(blank=False, null=False,
         help_text='The default priority of jobs in this queue: 0 = highest priority.')
 
@@ -16,17 +16,19 @@ class Configuration(models.Model):
         return self.watch_dir
 
 class JobQueue(models.Model):
-    input_file = models.CharField(max_length=100, blank=False, null=False,
+    JOB_QUEUE_STATUS_CHOICES = [
+        ('queued', 'Waiting in Queue'),
+        ('processing', 'Processing'),
+        ('success', 'Completed'),
+        ('fail', 'Failed')]
+    input_file = models.CharField(max_length=1024, blank=False, null=False,
         help_text='The file that was captured by inotfywait and made it through the filter.')
     priority = models.PositiveIntegerField(blank=False, null=False,
         help_text='The priority of this job: 0 = highest priority.')
     added = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=30, choices=[
-        ('queued', 'Waiting in Queue'),
-        ('processing', 'Processing'),
-        ('success', 'Completed'),
-        ('fail', 'Failed')])
+    configuration = models.ForeignKey(Configuration, blank=False, null=False)
+    status = models.CharField(max_length=30, choices=JOB_QUEUE_STATUS_CHOICES)
 
     def __unicode__(self):
         return self.input_file + ': ' + self.status
