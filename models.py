c81e42673ea7fa1722e298117b23917cf7b4f2df
Ian Llewellyn <ian_llewellyn@hotmail.com>
2013-04-11 11:40:36 +0100

    Moved classes out of queue_do-daemon.py and into models.py

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import subprocess # call
+import select # select
 
 # Create your models here.
 
@@ -32,3 +34,107 @@ class JobQueue(models.Model):
 
     def __unicode__(self):
         return self.input_file + ': ' + self.status
+
+class InotifyWait:
+    INOTIFYWAIT_CMD = 'inotifywait'
+    INOTIFYWAIT_ARGS = '--quiet --monitor --event CLOSE_WRITE --event MOVED_TO --format %w%f'
+    #INOTIFYWAIT_CMD = 'echo'
+    #INOTIFYWAIT_ARGS = '-q -m -e CLOSE_WRITE -f %f\ %w'
+    INOTIFYWAIT_READLINE_TIMEOUT = 0.1
+
+    def __init__(self, configuration=None):
+        # configuration
+        if configuration:
+            self.configuration = configuration
+        else:
+            return None
+        # Start 'er up...
+        self.start()
+
+    def start(self):
+        popen_args_tuple = (self.INOTIFYWAIT_CMD,)
+        popen_args_tuple += tuple(self.INOTIFYWAIT_ARGS.split())
+        if self.configuration.filter:
+            # ^.*\/\..*$ - This might be greedy and pick a hidden directory instead.
+            # ^.*\/\.[^\/]*$ - Ensure we are operating on the file part of the input.
+            popen_args_tuple += ('--exclude', self.configuration.filter)
+        popen_args_tuple += tuple(self.configuration.watch_dir.split(', '))
+        #print popen_args_tuple
+        self.process = subprocess.Popen(popen_args_tuple, stdout=subprocess.PIPE)
+
+    def stop(self):
+        self.process.terminate()
+
+    def readline(self):
+        # Two options here to stop the function blocking:
+        # 1. Use signal.alarm and register a dummy function
+        # 2. Use select.select with a timeout
+        (rlist, wlist, xlist) = select.select([self.process.stdout], [], [],
+            self.INOTIFYWAIT_READLINE_TIMEOUT)
+        if rlist:
+            #return self.process.stdout.readline()
+            return rlist[0].readline()
+        return ''
+
+class Processor(dict):
+    # 0 when idle, PID otherwise
+    CPU_IDLE_FLAG = None
+    def __init__(self, **kwargs):
+        self.__setitem__(0, self.CPU_IDLE_FLAG)
+        if kwargs.has_key('cpus'):
+            for i in range(kwargs['cpus']):
+                self.__setitem__(i, self.CPU_IDLE_FLAG)
+
+    def active(self):
+        for cpu_status in self.values():
+            return cpu_status != self.CPU_IDLE_FLAG
+        return False
+
+    def available(self):
+        self.check_jobs()
+        for cpu in self.keys():
+            cpu_status = self.get(cpu)
+            if cpu_status == self.CPU_IDLE_FLAG:
+                return True
+        return False
+
+    def check_jobs(self):
+        for cpu in self.keys():
+            cpu_status = self.get(cpu)
+            if cpu_status == self.CPU_IDLE_FLAG:
+                # No process on this CPU
+                continue
+            elif type(cpu_status['process'].poll()) == type(None):
+                # Process is still running
+                continue
+            elif cpu_status['process'].poll() == 0:
+                # The job completed successfully
+                cpu_status['job'].status = 'success'
+                cpu_status['job'].save()
+                self.__setitem__(cpu, self.CPU_IDLE_FLAG)
+            else:
+                # Failure of some sort
+                # Update job to Failed status and save
+                cpu_status['job'].status = 'fail'
+                cpu_status['job'].save()
+                self.__setitem__(cpu, self.CPU_IDLE_FLAG)
+
+    def start_job(self, job):
+        # Find the first unused CPU
+        for cpu, pid in self.items():
+            if pid == self.CPU_IDLE_FLAG:
+                break
+        # cpu is the processor to use
+        popen_args_tuple = tuple(job.configuration.run_script.split(' '))
+        popen_args_tuple += (job.input_file,)
+        #print popen_args_tuple
+        try:
+            job.status = 'processing'
+            self.__setitem__(cpu, {
+                'job': job,
+                'process': subprocess.Popen(popen_args_tuple)})
+        except:
+            job.status = 'fail'
+        finally:
+            job.save()
+
