d0eedffdd6897678571bc4d2f032b889ac706be6
root <root@dub-lx-087.rte.ie>
2013-04-13 17:21:03 +0100

    queue_do-daemon.py: Improvements to signal handling and related code.

--- management/commands/queue_do-daemon.py
+++ management/commands/queue_do-daemon.py
@@ -8,14 +8,10 @@ MAIN_LOOP_SLEEP_TIME = 1
 
 ## Signal Handling Functions
 def terminate(a, b):
-    # - send SIGTERM to all child processes
-    for inotify in inotifys:
-        inotify.stop()
     for cpu in processor:
         cpu_status = processor[cpu]
         if cpu_status == processor.CPU_IDLE_FLAG:
             continue
-        cpu_status['process'].terminate()
         # - set job status to fail
         cpu_status['job'].status = 'fail'
         cpu_status['job'].save()
@@ -36,22 +32,24 @@ def finish_current(a, b):
     # - exit
     sys.exit(0)
 
+## Read configuration from DB
+inotifys = []
+for configuration in Configuration.objects.all():
+    # Ignore SIGUSR1 - we use this for status
+    sigusr1_handler = signal.signal(signal.SIGUSR1, signal.SIG_IGN)
+    # Instanciate InotifyWait
+    inotifys.append(InotifyWait(configuration=configuration))
+
 ## Register Signal Handlers
 # SIGTERM:
 signal.signal(signal.SIGTERM, terminate)
 # Ctrl-C equivelant to SIGTERM
 signal.signal(signal.SIGINT, terminate)
-signal.signal(signal.SIGINT, finish_current)
 # SIGUSR2:
 signal.signal(signal.SIGUSR2, finish_current)
 
 ## Setup the processor flags:
 processor = Processor(cpus=CPUS)
-
-## Read configuration from DB
-inotifys = []
-for configuration in Configuration.objects.all():
-    inotifys.append(InotifyWait(configuration=configuration))
 
 jobs = True	# This will skip the DB query optimisation on the first run.
 running = True
@@ -99,8 +97,14 @@ while running:
 
     for job in jobs:
         if processor.available():
+            # Stop certain signals from getting to children
+            sigusr1_handler = signal.signal(signal.SIGUSR1, signal.SIG_IGN)
+            sigusr2_handler = signal.signal(signal.SIGUSR2, signal.SIG_IGN)
             # Start the job
             processor.start_job(job)
+            # Reset signal handlers
+            signal.signal(signal.SIGUSR1, sigusr1_handler)
+            signal.signal(signal.SIGUSR2, sigusr2_handler)
         else:
             # No processors are available
             break
