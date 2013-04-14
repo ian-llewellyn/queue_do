dafb7ca5a6b759a1bdd1c8c19b6cdcac56757b99
Ian Llewellyn <ian_llewellyn@hotmail.com>
2013-04-14 11:11:26 +0100

    admin.py: Further improvements to JobQueue in the admin interface. models.py: JobQueue now has property duration which is included in Admin interface too.

--- admin.py
+++ admin.py
@@ -4,8 +4,9 @@ from queue_do.models import Configuration, JobQueue
 admin.site.register(Configuration)
 
 class JobQueueAdmin(admin.ModelAdmin):
-    list_display = ['priority', 'input_file', 'added', 'modified', 'status']
+    list_display = ['priority', 'input_file', 'added', 'duration', 'status']
     list_display_links = ['input_file', ]
-    search_fields = ('input_file', )
-    list_filter = ('status', 'configuration', 'priority', )
+    search_fields = ['input_file', ]
+    list_filter = ['status', 'configuration', 'priority']
+    readonly_fields = ['added', 'modified', 'duration']
 admin.site.register(JobQueue, JobQueueAdmin)
