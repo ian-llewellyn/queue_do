6fa901f7378e4123334a64398c273bca9688e9e2
Ian Llewellyn <ian_llewellyn@hotmail.com>
2013-04-14 10:52:43 +0100

    DOC: Added more things to do. admin.py: Introduced JobQueueAdmin with list_display_links to pretty it up.

--- admin.py
+++ admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from queue_do.models import Configuration, JobQueue
 
 admin.site.register(Configuration)
-admin.site.register(JobQueue)
+
+class JobQueueAdmin(admin.ModelAdmin):
+    list_display = ['priority', 'input_file', 'added', 'modified', 'status']
+    list_display_links = ['input_file', ]
+    search_fields = ('input_file', )
+    list_filter = ('status', 'configuration', 'priority', )
+admin.site.register(JobQueue, JobQueueAdmin)
